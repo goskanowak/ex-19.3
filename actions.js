1c756db1b44086de5e93374f3a2089c29ee0a619
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-05 12:30:40 +0200

    Add reducer

--- actions.js
+++ actions.js
@@ -1,45 +1,45 @@
 import uuid from uuid;
 
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-function addComment(text) {
+const addComment = function addComment(text) {
   return {
     type: ADD_COMMENT,
     text,
     id: uuid.v4()
-}
+};
 
-function removeComment(id) {
+const removeComment = function removeComment(id) {
   return {
     type: REMOVE_COMMENT,
     id
   }
-}
+};
     
-function editComment(id) {
+const editComment = function editComment(id) {
   return {
     type: EDIT_COMMENT,
     id
   } 
-}
+};
     
-function thumbUpComment(id) {
+const thumbUpComment = function thumbUpComment(id) {
   return {
     type: THUMB_UP_COMMENT,
     id: uuid.v4()
   }
-}
+};
 
-function thumbDownComment(id) {
+const thumbDownComment = function thumbDownComment(id) {
   return {
     type: THUMB_DOWN_COMMENT,
     id: uuid.v4()
   }
-}
+};
     
 const boundAddComment = text => dispatch(addComment(text));
 const boundRemoveComment = id => dispatch(removeComment(id));
---
+++ comments.js
@@ -0,0 +1,52 @@
+import ADD_COMMENT from './actions' ;
+import REMOVE_COMMENT from './actions' ;
+import EDIT_COMMENT from './actions';
+import THUMB_UP_COMMENT from './actions' ;
+import THUMB_DOWN_COMMENT from './actions' ;
+
+export function comments(state = [], action) {
+  switch(action.type) {
+    case ADD_COMMENT:
+      return [{  
+            id: action.id,
+            text: action.text,
+            votes: 0
+          }
+          , ...state.comments];
+          
+    case REMOVE_COMMENT:
+      return state.comments.filter(comment => comment.id !== action.id) // w metodzie filter przekazujemy callback, który jest wykonywany po każdym el tablicy comments, odsiewa te które nie spełniają warunku tego samego id.
+          
+    case EDIT_COMMENT:
+      return state.comments.filter(comment => {
+          if (comment.id === action.id) {
+            return Object.assign({}, comment, {
+              text: action.text
+            });
+          } else {
+            return comment;
+          };
+      });
+          
+    case THUMB_DOWN_COMMENT:
+      return state.comments.filter(comment => {
+        if (comment.id === action.id) {
+          return Object.assign({}, comment, {
+            votes: action.votes
+          });
+        } else {
+          return comment;
+        }
+      });
+          
+    case THUMB_UP_COMMENT: 
+      return state.comments.filter(comment => {
+        if (comment.id === action.id) {
+          return Object.assign({}, comment, {
+            votes: action.votes 
+          });
+        } else {
+          return comment;
+        }
+      });
+};
